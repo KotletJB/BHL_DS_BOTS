0fae508d64a0e4fb94fbbbe532085454c8b1e589
KotletJB <bronowski.jan2@gmail.com>
2020-12-29 09:54:03 +0100

    bot_text

--- bot1.py
+++ bot1.py
@@ -11,4 +11,14 @@ with open('../token_BHL_DS.txt') as f:
 async def on_message(message):
     print(message.content)
 
+    with open('responde_massage.csv') as f:
+        x = f.readlines()
+        key_words = {}
+        for element in x:
+            key, value = element.strip().split(',')
+            key_words[key] = value
+
+    if message.content in key_words:
+        await message.channel.send(key_words[massage.content])
+        
 bot.run(token)
